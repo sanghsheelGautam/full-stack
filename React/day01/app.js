eb61359622622d468d653fef5d2159098e7783f1
Sanghsheel Gautam <badshahrock222@gmail.com>
2026-02-17 02:14:44 +0530

    New folder add

---
+++ React/day01/app.js
@@ -0,0 +1,5 @@
+const element = React.createElement("h1",{},"Hello coder army");
+// ReactDOM.Render(element,document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element)
